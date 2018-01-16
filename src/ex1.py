51ab9b9a7043aeafe98bbdbde3b6e0be50771526
zhangjiliang <zhangjiliang900123@163.com>
2018-01-16 23:34:27 +0800

    ex1

--- src/ex1.py
+++ src/ex1.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
-print "hello 哈哈"
+print "hello World! 哈哈"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+print "六朝旧事随流水，但寒烟衰草凝绿"
