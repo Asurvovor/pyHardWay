b8ab3872178bc197d151c7ee01d1575c471f357a
zhangjiliang <zhangjiliang@guazi.com>
2018-01-29 17:18:57 +0800

    string boolen float

---
+++ liao/string.py
@@ -0,0 +1,31 @@
+print(r'\(~_~)/\(~_~)/')
+
+s = r'''line 1
+line 2
+line 3 '''
+print(s)
+
+s1 = r'''Python is created by "Guido".
+It is free and easy to learn.
+Let's stert learn Python in imooc!'''
+print(s1)
+
+s2 = '''"To be, or not to be": that is the question.
+Whether it's nobler in the mind to suffer.'''
+print (s2)
+
+print ('''
+静夜思
+
+床前明月光
+疑是地上霜
+举头望明月
+低头思故乡
+''')
+
+print(2.5 + 10/4)
+
+a = 'Python'
+print('hello', a or 'world')
+b = ''
+print('hello', b or 'world')
