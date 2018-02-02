c2ce5b163c6848a71ac2031a94484c299f955e56
zhangjiliang <zhangjiliang@guazi.com>
2018-02-02 18:10:02 +0800

    list tuple

---
+++ liao/list.py
@@ -0,0 +1,26 @@
+# list tuple
+
+list1  = ['tom', 'jack', 'lucy']
+
+print(list1)
+
+print(list1[-1])
+
+list1.append('lisa')
+print(list1)
+
+list1.insert(1, 'mike')
+print(list1)
+
+list1.pop(0)
+print(list1)
+
+list1[2] = 'ann'
+print(list1)
+
+
+tuple1 = ('bill gates', 'magina', ['jugg', 'lina'])
+print(tuple1)
+print(tuple1[1])
+tuple1[2][0] = 'miyala'
+print(tuple1)
